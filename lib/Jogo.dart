622b86a43a6d71cc8b84c0f795f43cf5d5ddcf17
Fabio Antunes de Vasconcelos <fabioantunes@brq.com>
2019-11-22 19:40:42 -0300

    Inicio do aplicativo - Scaffold

---
+++ lib/Jogo.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JoKenPo"),
+      ),
+      body: ,
+    );
+  }
+}
